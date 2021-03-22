ff42645b98c97152be8c604cbc5fb263ba9e90a7
skenderskiantonio <skenderskiantonio@students.softuni.bg>
2021-03-22 08:53:38 +0200

    Added Header

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,8 @@
+import {Layout} from "antd";
+import NavigationBar from "../NavigationBar/NavigationBar";
+
+export default function () {
+    return (
+        <Layout.Header><NavigationBar /></Layout.Header>
+    );
+}
