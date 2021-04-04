590f9888c9a5d775138fddebd75a6c579f99a4ad
skenderskiantonio <skenderskia@gmail.com>
2021-04-05 00:07:34 +0300

    Added Add Node functionality to NodesCard

--- client/src/Components/Content/NodesCard/NodesCard.js
+++ client/src/Components/Content/NodesCard/NodesCard.js
@@ -2,6 +2,9 @@ import Nodes from "../Nodes/Nodes";
 
 export default function NodesCard({nodes, handlers}) {
     return (
-        <Nodes nodes={nodes} handlers={handlers} />
+        <>
+            <button onClick={handlers.addNode}>Add Node</button>
+            <Nodes nodes={nodes} handlers={{setNode: handlers.setNode}} />
+        </>
     );
 };
--- client/src/Tests/Components/Content/NodesCard.test.js
+++ client/src/Tests/Components/Content/NodesCard.test.js
@@ -1,14 +1,19 @@
 import NodesCard from "../../../Components/Content/NodesCard/NodesCard";
 import {mount} from "enzyme";
 import Nodes from "../../../Components/Content/Nodes/Nodes";
+import {fireEvent, render} from "@testing-library/react";
+
+const defaultHandlers = { setNode: () => {}, addNode: () => {} };
 
 jest.mock("../../../Components/Content/Nodes/Nodes", () => jest.fn());
 
-test("NodesCard contains Nodes component with correct props", () => {
+beforeEach(()=>{
     Nodes.mockImplementation(()=>null);
+});
 
+test("NodesCard contains Nodes component with correct props", () => {
     const nodes = {};
-    const handlers = {};
+    const handlers = defaultHandlers;
 
     const wrapper = mount(<NodesCard nodes={nodes} handlers={handlers} />);
 
@@ -17,5 +22,18 @@ test("NodesCard contains Nodes component with correct props", () => {
     expect(nodesComponent).toHaveLength(1);
 
     expect(nodesComponent.prop("nodes")).toBe(nodes);
-    expect(nodesComponent.prop("handlers")).toBe(handlers);
-})
+
+    const handlersOfNodesComponent = nodesComponent.prop("handlers");
+    expect(nodesComponent.prop("handlers")).toEqual({setNode: handlers.setNode});
+});
+
+test("NodesCard adds node when Add Node button is clicked", () => {
+    const nodes = {};
+    const handlers = {...defaultHandlers, addNode: jest.fn()};
+
+    const wrapper = render(<NodesCard nodes={nodes} handlers={handlers} />);
+
+    fireEvent.click(wrapper.getByText("Add Node"));
+
+    expect(handlers.addNode).toHaveBeenCalledTimes(1);
+});
