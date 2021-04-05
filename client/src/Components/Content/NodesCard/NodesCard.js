00ca0abf68fefd77746d554e24f7eb0079d88c76
skenderskiantonio <skenderskia@gmail.com>
2021-04-05 12:30:46 +0300

    Added Delete Node functionality to NodesCard component

--- client/src/Components/Content/NodesCard/NodesCard.js
+++ client/src/Components/Content/NodesCard/NodesCard.js
@@ -1,10 +1,17 @@
 import Nodes from "../Nodes/Nodes";
+import useToggle from "../../../Hooks/useToggle";
 
 export default function NodesCard({nodes, handlers}) {
+    const [isDeleting, toggleIsDeleting] = useToggle(false);
+
     return (
         <>
             <button onClick={handlers.addNode}>Add Node</button>
-            <Nodes nodes={nodes} handlers={{setNode: handlers.setNode}} />
+            <button onClick={toggleIsDeleting}>Delete Node</button>
+            <Nodes nodes={nodes} handlers={{setNode: handlers.setNode, deleteNode: (...props) => {
+                toggleIsDeleting();
+                handlers.deleteNode(...props);
+                }}} isDeletingNode={isDeleting} />
         </>
     );
 };
--- client/src/Tests/Components/Content/NodesCard.test.js
+++ client/src/Tests/Components/Content/NodesCard.test.js
@@ -1,10 +1,11 @@
 import NodesCard from "../../../Components/Content/NodesCard/NodesCard";
 import {mount} from "enzyme";
 import Nodes from "../../../Components/Content/Nodes/Nodes";
-import {fireEvent, render} from "@testing-library/react";
+import {act, fireEvent, render} from "@testing-library/react";
 
-const defaultHandlers = { setNode: () => {}, addNode: () => {} };
+const defaultHandlers = { setNode: () => {}, addNode: () => {}, deleteNode: () => {} };
 
+//useToggle is presumed to be working and so it is not mocked
 jest.mock("../../../Components/Content/Nodes/Nodes", () => jest.fn());
 
 beforeEach(()=>{
@@ -24,7 +25,10 @@ test("NodesCard contains Nodes component with correct props", () => {
     expect(nodesComponent.prop("nodes")).toBe(nodes);
 
     const handlersOfNodesComponent = nodesComponent.prop("handlers");
-    expect(nodesComponent.prop("handlers")).toEqual({setNode: handlers.setNode});
+    //setNode shouldn't be changed
+    expect(handlersOfNodesComponent).toMatchObject({ setNode: handlers.setNode });
+    //deleteNode is changed and so we cannot match for reference, but simply to mak sure it's passed down
+    expect(handlersOfNodesComponent.deleteNode).not.toBeUndefined();
 });
 
 test("NodesCard adds node when Add Node button is clicked", () => {
@@ -36,4 +40,99 @@ test("NodesCard adds node when Add Node button is clicked", () => {
     fireEvent.click(wrapper.getByText("Add Node"));
 
     expect(handlers.addNode).toHaveBeenCalledTimes(1);
+});
+
+test("NodesCard has delete button", () => {
+    const nodes = {};
+    const handlers = defaultHandlers;
+
+    const wrapper = render(<NodesCard nodes={nodes} handlers={handlers} />);
+
+    const deleteNodesButton = wrapper.getByText("Delete Node");
+
+    expect(deleteNodesButton).toBeInTheDocument();
+});
+
+test("NodesCard puts Nodes component in delete mode when delete button is clicked", () => {
+    let nodesProps = {};
+    Nodes.mockImplementation(props=>{
+        nodesProps = props;
+        return null;
+    });
+
+    const nodes = {};
+    const handlers = defaultHandlers;
+
+    const wrapper = render(<NodesCard nodes={nodes} handlers={handlers} />);
+
+    const deleteNodesButton = wrapper.getByText("Delete Node");
+
+    fireEvent.click(deleteNodesButton);
+
+    expect(nodesProps.isDeletingNode).toBe(true);
+});
+
+test("NodesCard stops Nodes component from being in delete mode when delete button is clicked twice", () => {
+    let nodesProps = {};
+    Nodes.mockImplementation(props=>{
+        nodesProps = props;
+        return null;
+    });
+
+    const nodes = {};
+    const handlers = defaultHandlers;
+
+    const wrapper = render(<NodesCard nodes={nodes} handlers={handlers} />);
+
+    const deleteNodesButton = wrapper.getByText("Delete Node");
+
+    fireEvent.click(deleteNodesButton);
+    fireEvent.click(deleteNodesButton);
+
+    expect(nodesProps.isDeletingNode).toBe(false);
+});
+
+test("NodesCard stops Nodes component from being in delete mode when node is deleted", () => {
+    let nodesProps = {};
+
+    Nodes.mockImplementation(props=>{
+        nodesProps = props;
+
+        return null;
+    });
+
+    const nodes = {};
+    const handlers = defaultHandlers;
+
+    const wrapper = render(<NodesCard nodes={nodes} handlers={handlers} />);
+
+    const deleteNodesButton = wrapper.getByText("Delete Node");
+
+    fireEvent.click(deleteNodesButton);
+    act(()=>{
+        nodesProps.handlers.deleteNode();
+    });
+
+    expect(nodesProps.isDeletingNode).toBe(false);
+});
+
+test("NodesCard passes deleteNode arguments successfully to parent handler when node is deleted", () => {
+    let deleteNodeArgs;
+    const deleteNodeExpectedArgs = [1, 2 ,3];
+
+    Nodes.mockImplementation(props=>{
+        return null;
+    });
+
+    const nodes = {};
+    const handlers = {...defaultHandlers, deleteNode: (...props)=> deleteNodeArgs = props};
+
+    const wrapper = render(<NodesCard nodes={nodes} handlers={handlers} />);
+
+    const deleteNodesButton = wrapper.getByText("Delete Node");
+
+    fireEvent.click(deleteNodesButton);
+    handlers.deleteNode(...deleteNodeExpectedArgs);
+
+    expect(deleteNodeArgs).toEqual(deleteNodeExpectedArgs);
 });
