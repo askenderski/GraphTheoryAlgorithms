449f8f0d43d47c87e669a6c5c00c08dbef3310c5
skenderskiantonio <skenderskia@gmail.com>
2021-04-06 22:49:14 +0300

    Added ControlledTextInputWithValidation

---
+++ client/src/Components/Common/ControlledTextInputWithValidation/ControlledTextInputWithValidation.js
@@ -0,0 +1,6 @@
+import withValidateOnInputStop from "../../../Hooks/withValidateOnInputStop";
+import ControlledTextInput from "../ControlledTextInput/ControlledTextInput";
+
+const ControlledTextInputWithValidation = withValidateOnInputStop(ControlledTextInput);
+
+export default ControlledTextInputWithValidation;
