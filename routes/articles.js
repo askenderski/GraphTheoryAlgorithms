80438cb04304af980d2f92abf3c4c0e494c68f3f
skenderskiantonio <skenderskia@gmail.com>
2021-04-07 23:11:46 +0300

    Removed log for testing purposes

--- routes/articles.js
+++ routes/articles.js
@@ -7,7 +7,6 @@ router.get("/:algorithmTypeId/:algorithmId", async (req, res) => {
 
         const article = await ArticleModel.findOne({type: algorithmTypeId, title: algorithmId});
 
-        console.log(article)
         res.status(200).json(article);
     } catch(err) {
         throw {status: 404, message: "Article does not exist"};
