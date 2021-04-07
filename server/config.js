67b5c2df4555f3d1d8b9f38a9f6342531d2e4744
skenderskiantonio <skenderskia@gmail.com>
2021-04-07 17:59:44 +0300

    Removing server to separate repo

--- server/config.js
+++
@@ -1,7 +0,0 @@
-function config() {
-    if (process.env.NODE_ENV !== 'production') {
-        require('dotenv').config();
-    }
-}
-
-module.exports = config;
--- server/routing.js
+++
@@ -1,5 +0,0 @@
-const router = require("express").Router();
-
-router.use("/auth", require("./routes/authentication"));
-
-module.exports = router;
