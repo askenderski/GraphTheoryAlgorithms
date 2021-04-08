9ddfa3f14ec22032387faed151bb1b4e64c3bc10
skenderskiantonio <skenderskia@gmail.com>
2021-04-08 21:20:56 +0300

    Added cors options

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ require("./configDatabase")();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 
