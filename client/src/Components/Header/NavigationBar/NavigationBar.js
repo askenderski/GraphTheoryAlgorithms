aa2ee26ca5a2b974b29789a571be2540ce7c9187
skenderskiantonio <skenderskia@gmail.com>
2021-04-08 03:44:11 +0300

    Added UserContext in NavigationBar

--- client/src/Components/Header/NavigationBar/NavigationBar.js
+++ client/src/Components/Header/NavigationBar/NavigationBar.js
@@ -1,10 +1,35 @@
 import navigationBarItems from "../../../Data/NavigationBarItems";
 import {Menu} from "antd";
 
+import UserContext from "../../../Contexts/User";
+import {useContext} from "react";
+
 export default function () {
+    const user = useContext(UserContext);
+
+    const userNavigation = user === undefined
+        ? (
+            <>
+                <Menu.Item style={{float: "right"}}>
+                    Register
+                </Menu.Item>
+                <Menu.Item style={{float: "right"}}>
+                    Login
+                </Menu.Item>
+            </>
+        )
+        : (
+            <>
+                <Menu.Item style={{float: "right"}}>
+                    {user.username}
+                </Menu.Item>
+            </>
+        );
+
     return (
         <Menu mode="horizontal">
             {navigationBarItems.map(item=><Menu.Item key={item.text}>{item.text}</Menu.Item>)}
+            {userNavigation}
         </Menu>
     );
 }
