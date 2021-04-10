6c9b64b290e8099a7c276b852f8ece730deee193
skenderskiantonio <skenderskia@gmail.com>
2021-04-10 14:57:16 +0300

    Added isDirected and isWeighted in NodesCard

--- client/src/Components/Content/AlgorithmPage/NodesCard/NodesCard.js
+++ client/src/Components/Content/AlgorithmPage/NodesCard/NodesCard.js
@@ -1,5 +1,6 @@
 import Nodes from "./Nodes/Nodes";
 import useToggle from "../../../../Hooks/useToggle";
+import ToggleableComponent from "../../../Common/ToggleableComponent/ToggleableComponent";
 
 export default function NodesCard({nodes, handlers}) {
     const [isDeleting, toggleIsDeleting] = useToggle(false);
@@ -8,9 +9,19 @@ export default function NodesCard({nodes, handlers}) {
         <>
             <button onClick={handlers.addNode}>Add Node</button>
             <button onClick={toggleIsDeleting}>Delete Node</button>
+            <ToggleableComponent
+                isOn={nodes.isDirected}
+                toggleIsOn={handlers.toggleIsDirected}
+                values={{on: "Directed", off: "Undirected"}}
+            />
+            <ToggleableComponent
+                isOn={nodes.isWeighted}
+                toggleIsOn={handlers.toggleIsWeighted}
+                values={{on: "Weighted", off: "Unweighted"}}
+            />
             <Nodes nodes={nodes} handlers={{setNode: handlers.setNode, deleteNode: (...props) => {
-                toggleIsDeleting();
-                handlers.deleteNode(...props);
+                    toggleIsDeleting();
+                    handlers.deleteNode(...props);
                 }}} isDeletingNode={isDeleting} />
         </>
     );
