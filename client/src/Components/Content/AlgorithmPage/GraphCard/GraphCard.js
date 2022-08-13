ce6ca030a002c68918a431a11b9c395d5d947d94
askenderski <skenderskia@gmail.com>
2022-08-13 22:43:39 +0300

    Added Move and Resize func to GraphCard

--- client/src/Components/Content/AlgorithmPage/GraphCard/GraphCard.js
+++ client/src/Components/Content/AlgorithmPage/GraphCard/GraphCard.js
@@ -1,10 +1,25 @@
 import GraphContainer from "./GraphContainer/GraphContainer";
 import style from "./GraphCard.module.css";
+import cardStyleModule from "../../../Common/Card/Card.module.css";
+import useMover from "../../../../Hooks/useMover";
+import { getCardStyle, getHeaderStyle } from "../NodesCard/Utilities/getStyles";
+import useResizer from "../../../../Hooks/useResizer";
+
+const defaultPosition = {x: 300, y: 300};
+const defaultSize = {width: "300px", height: "300px"};
 
 export default function GraphCard(props) {
+    const {position, onMouseDown} = useMover(defaultPosition);
+    const {ref, size} = useResizer(defaultSize);
+
+    const headerStyle = getHeaderStyle({size, position});
+    const cardStyle = getCardStyle({size, position});
+
     return (
-        <div className={style.card}>
-            <GraphContainer/>
+        <div ref={ref} className={`${style.card} ${cardStyleModule.card}`} style={cardStyle}>
+            <div className={cardStyleModule.header} onMouseDown={onMouseDown} style={headerStyle}>Graph</div>
+            <div style={{...headerStyle, flexShrink: 0}}/>
+            <div style={{height: "100%", width: "100%"}}><GraphContainer/></div>
         </div>
     );
 }
--- client/src/Components/Content/AlgorithmPage/NodesCard/NodesCard.js
+++ client/src/Components/Content/AlgorithmPage/NodesCard/NodesCard.js
@@ -1,4 +1,5 @@
 import style from "./NodesCard.module.css";
+import cardStyleModule from "../../../Common/Card/Card.module.css";
 import useResizer from "../../../../Hooks/useResizer";
 import NodesCardBody from "./NodesCardBody/NodesCardBody";
 import { getCardStyle, getHeaderStyle } from "./Utilities/getStyles";
@@ -7,7 +8,7 @@ import useMover from "../../../../Hooks/useMover";
 const defaultPosition = {x: 100, y: 100};
 const defaultSize = {width: "300px", height: "300px"};
 
-const headerClasses = `${style.cardHeader} ${style.unselectable}`;
+const headerClasses = `${cardStyleModule.header} ${style.unselectable}`;
 
 export default function NodesCard({nodes, handlers}) {
     const {position, onMouseDown} = useMover(defaultPosition);
@@ -18,7 +19,7 @@ export default function NodesCard({nodes, handlers}) {
 
     return (
         // sizeRef is used for tracking card size and therefore changing the size of the header
-        <div className={style.card} ref={sizeRef} style={cardStyle}>
+        <div className={cardStyleModule.card} ref={sizeRef} style={cardStyle}>
             {/* onMouseDown is used for detecting when the header is being moved */}
             <div onMouseDown={onMouseDown} className={headerClasses} style={headerStyle}>
                 Graph
