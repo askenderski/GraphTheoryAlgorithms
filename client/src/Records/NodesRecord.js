5a5f05d7926ab76d76b15abd3a466e93c467a7a6
askenderski <skenderskia@gmail.com>
2022-08-14 21:47:55 +0300

    Extracted handler functionality into NodesRecord

--- client/src/Records/NodesRecord.js
+++ client/src/Records/NodesRecord.js
@@ -1,5 +1,20 @@
 import {List, Record} from "immutable";
 
+function directedToUndirectedNodeMatrix(directedNodeMatrix) {
+    let matrix = directedNodeMatrix;
+
+    for (let fromIndex = 0; fromIndex < directedNodeMatrix.size; fromIndex++) {
+        for (let toIndex = 0; toIndex < directedNodeMatrix.size; toIndex++) {
+            const cellValue = matrix.get(toIndex).get(fromIndex);
+            console.log(cellValue, toIndex, fromIndex)
+
+            if (cellValue) matrix = matrix.setIn([fromIndex, toIndex], cellValue);
+        }
+    }
+
+    return matrix;
+}
+
 const nodesRecord = Record(
     {
         nodeCount: 1,
@@ -13,6 +28,92 @@ nodesRecord.prototype.getNode = function ({i, j}) {
 
     return matrix.get(i).get(j);
 };
+
+nodesRecord.prototype.addNode = function () {
+    const defaultNode = this.get("isWeighted") ? 0 : false;
+    const nodesWithNewCount = this.set("nodeCount", this.nodeCount + 1);
+
+    let nodeMatrix = nodesWithNewCount.get("nodeMatrix");
+    //every column started by a to node with from nodes will add a new node at the end,
+    //that being the newly added node, and it will have the default (0 or false) value
+    let fromNodesMatrix = new List();
+
+    for (let nodeColumn of nodeMatrix) {
+        //the current node column will add the default node at the end
+        const nodeColumnWithNewFromNode = nodeColumn.push(defaultNode);
+
+        fromNodesMatrix = fromNodesMatrix.push(nodeColumnWithNewFromNode);
+    }
+
+    nodeMatrix = fromNodesMatrix;
+
+    //the last column (the one where the destination is the new node) is empty as it isn't needed
+    const columnOfNodesFromToNodeTo = List.of(...new Array(nodesWithNewCount.nodeCount).fill(defaultNode));
+
+    nodeMatrix = nodeMatrix.push(columnOfNodesFromToNodeTo);
+
+    return nodesWithNewCount.set("nodeMatrix", nodeMatrix);
+}
+
+nodesRecord.prototype.setNode = function({i, j}, val) {
+    const fromJToI = ["nodeMatrix", i, j];
+    const fromIToJ = ["nodeMatrix", j, i];
+
+
+    if (!this.get("isDirected")) {
+        return this.setIn(fromJToI, val).setIn(fromIToJ, val);
+    }
+
+    return this.setIn(fromJToI, val);
+}
+
+nodesRecord.prototype.toggleIsWeighted = function() {
+    const wasWeighted = this.get("isWeighted");
+    const nodesWithReversedWeight = this.set("isWeighted", !wasWeighted);
+
+    const changeNodeValueDependingOnWeightedness = wasWeighted ?
+        //if the graph was weighted, the existing edges remain so
+        nodeCell => nodeCell !== 0 ? true : false :
+        //if the graph wasn't weighted, the "true" edges become 1s
+        nodeCell => nodeCell ? 1 : 0;
+
+    const nodeMatrixWithReversedWeight = nodesWithReversedWeight
+        .get("nodeMatrix")
+        .map(nodeColumn=>
+                nodeColumn
+                .map(edge=>edge ? 1 : 0)
+            );
+
+    return nodesWithReversedWeight.set("nodeMatrix", nodeMatrixWithReversedWeight);
+}
+
+nodesRecord.prototype.deleteNode = function (i) {
+    if (this.nodeCount <= 0) return this;
+
+    const nodesWithNewCount = this.set("nodeCount", this.nodeCount - 1);
+
+    const nodeMatrixWithoutFromCells = this.get("nodeMatrix")
+        .reduce((curList, nodeRow) => {
+            return curList.push(nodeRow.delete(i));
+        }, List());
+
+    const nodeMatrixWithoutFromCellsAndToCell = nodeMatrixWithoutFromCells.delete(i);
+
+    return nodesWithNewCount.set("nodeMatrix", nodeMatrixWithoutFromCellsAndToCell);
+}
+
+nodesRecord.prototype.toggleIsDirected = function () {
+    const wasDirected = this.get("isDirected");
+    const nodesWithReversedDirected = this.set("isDirected", !wasDirected);
+
+    if (wasDirected) {
+        return nodesWithReversedDirected.set("nodeMatrix",
+            directedToUndirectedNodeMatrix(this.get("nodeMatrix"))
+        );
+    }
+
+    return nodesWithReversedDirected;
+}
 
 function NodeMatrixAsListFromArray(nodeMatrixArray) {
     return List.of(...nodeMatrixArray.map(nodeRowArray=>List.of(...nodeRowArray)));
--- client/src/Tests/Utilities/algorithmHandlers.js
+++ client/src/Tests/Utilities/algorithmHandlers.js
@@ -2,87 +2,23 @@ import {List} from "immutable";
 
 export function getHandlers(nodes, setNodes) {
     function addNode() {
-        setNodes(nodes => {
-            const defaultNode = nodes.get("isWeighted") ? 0 : false;
-            const nodesWithNewCount = nodes.set("nodeCount", nodes.nodeCount + 1);
-
-            return nodesWithNewCount.set("nodeMatrix", nodes.get("nodeMatrix")
-                .reduce((curList, nodeRow) => {
-                    return curList.push(nodeRow.push(defaultNode));
-                }, List())
-                .push(List.of(...new Array(nodesWithNewCount.nodeCount).fill(defaultNode))));
-        });
+        setNodes(nodes => nodes.addNode());
     }
 
     function setNode({i, j}, val) {
-        setNodes(nodes => {
-            if (!nodes.get("isDirected")) {
-                return nodes.setIn(["nodeMatrix", i, j], val).setIn(["nodeMatrix", j, i], val);
-            }
-
-            return nodes.setIn(["nodeMatrix", i, j], val);
-        });
+        setNodes(nodes => nodes.setNode({i, j}, val));
     }
 
     function deleteNode(i) {
-        setNodes(nodes => {
-            if (nodes.nodeCount <= 0) return nodes;
-
-            const nodesWithNewCount = nodes.set("nodeCount", nodes.nodeCount - 1);
-
-            return nodesWithNewCount.set("nodeMatrix", nodes.get("nodeMatrix")
-                .reduce((curList, nodeRow) => {
-                    return curList.push(nodeRow.delete(i));
-                }, List())
-                .delete(i));
-        });
-    }
-
-    function undirectedToDirectedNodeMatrix(undirectedNodeMatrix) {
-        return undirectedNodeMatrix
-            .reduce((curMatrix, row, rowIndex) => {
-                return row.slice(rowIndex)
-                    .reduce(
-                        (newFinalMatrix, curCell, cellIndex) =>
-                            newFinalMatrix.setIn(
-                                [cellIndex+rowIndex, rowIndex],
-                                curCell
-                            ),
-                        curMatrix
-                    );
-            }, undirectedNodeMatrix);
+        setNodes(nodes => nodes.deleteNode(i));
     }
 
     function toggleIsDirected() {
-        setNodes(nodes => {
-            const wasDirected = nodes.get("isDirected");
-            const nodesWithReversedDirected = nodes.set("isDirected", !wasDirected);
-
-            if (wasDirected) {
-                return nodesWithReversedDirected.set("nodeMatrix",
-                    undirectedToDirectedNodeMatrix(nodes.get("nodeMatrix"))
-                );
-            }
-
-            return nodesWithReversedDirected;
-        });
+        setNodes(nodes => nodes.toggleIsDirected());
     }
 
     function toggleIsWeighted() {
-        setNodes(nodes => {
-            const wasWeighted = nodes.get("isWeighted");
-            const nodesWithReversedWeight = nodes.set("isWeighted", !wasWeighted);
-
-            if (wasWeighted) {
-                return nodesWithReversedWeight.set("nodeMatrix",
-                    nodesWithReversedWeight.get("nodeMatrix")
-                        .map(nodeRow=>nodeRow.map(nodeCell=>nodeCell !== 0 ? true : false)));
-            } else {
-                return nodesWithReversedWeight.set("nodeMatrix",
-                    nodesWithReversedWeight.get("nodeMatrix")
-                        .map(nodeRow=>nodeRow.map(nodeCell=>nodeCell ? 1 : 0)));
-            }
-        });
+        setNodes(nodes => nodes.toggleIsWeighted());
     }
 
     return {addNode, setNode, deleteNode, toggleIsDirected, toggleIsWeighted};
