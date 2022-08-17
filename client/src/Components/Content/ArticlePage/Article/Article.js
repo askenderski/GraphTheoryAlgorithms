c21e52391755ccca3ddf6458d7fc5264fc1e0311
askenderski <skenderskia@gmail.com>
2022-08-17 16:09:55 +0300

    Added button to article that redirect to algorithm

--- client/src/Components/Content/ArticlePage/Article/Article.js
+++ client/src/Components/Content/ArticlePage/Article/Article.js
@@ -1,6 +1,7 @@
-import React from "react";
+import {useHistory} from "react-router-dom";
 import {articleColorsMap, articleTextMap} from "../../../../Data/ArticleTypes";
 import {validateArticleType} from "../../../../Utilities/Validation/articleTypeValidation";
+import {Routes} from "../../../../Data/Routes/routes";
 
 function validateProps({title, description, type}) {
     if (typeof title !== "string" || title === "") {
@@ -16,12 +17,21 @@ function validateProps({title, description, type}) {
 
 export default function Article(props) {
     validateProps(props);
+    const {type, title, description} = props;
+
+    const history = useHistory();
+
+    const algorithmPath = Routes.algorithms.algorithmType.algorithm.fullPath
+        .replace(":algorithmType", type.name)
+        .replace(":algorithmTitle", title);
+    const redirectToAlgorithm = ()=>history.push(algorithmPath);
 
     return (
         <div>
-            <h1 style={{backgroundColor: articleColorsMap.get(props.type)}}>{articleTextMap.get(props.type)}</h1>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
+            <h1 style={{backgroundColor: articleColorsMap.get(type)}}>{articleTextMap.get(type)}</h1>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <button onClick={redirectToAlgorithm}>Go to algorithm</button>
         </div>
     );
 };
