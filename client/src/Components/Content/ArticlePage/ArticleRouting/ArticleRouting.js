1ff89288aa46632643cdd56a584eba02765e2d0d
askenderski <skenderskia@gmail.com>
2022-08-17 15:42:50 +0300

    Slightly refactored ArticleRouting

--- client/src/Components/Content/ArticlePage/ArticleRouting/ArticleRouting.js
+++ client/src/Components/Content/ArticlePage/ArticleRouting/ArticleRouting.js
@@ -3,10 +3,6 @@ import Article from "../Article/Article";
 import {useEffect, useState} from "react";
 import Loading from "../../../Common/Loading/Loading";
 
-function getArticleComponent(article) {
-    return <Article title={article.title} description={article.description} type={article.type}/>;
-}
-
 export default function ArticleRouting({match}) {
     const [articleExists, setArticleExists] = useState(true);
     const [article, setArticle] = useState(undefined);
@@ -19,7 +15,7 @@ export default function ArticleRouting({match}) {
 
     if (articleExists) {
         if (article !== undefined) {
-            return getArticleComponent(article);
+            return <Article title={article.title} description={article.description} type={article.type}/>;
         } else {
             return <Loading/>;
         }
