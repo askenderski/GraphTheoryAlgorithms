7e4b29fcec8a514f59f311d51909fb4310a15555
askenderski <skenderskia@gmail.com>
2022-08-19 17:16:35 +0300

    Separated algorithm handlers

--- client/src/Components/Content/Algorithms/General/TopSort.js
+++ client/src/Components/Content/Algorithms/General/TopSort.js
@@ -1,23 +1,21 @@
 import NodeContext from "../../../../Contexts/Controller/Node";
 import NodesCard from "../../../Common/Algorithms/NodesCard/NodesCard";
-import { getHandlers } from "../../../../Utilities/algorithmHandlers";
+import { getNodesHandlers, getStartAlgorithmHandlers } from "../../../../Utilities/algorithmHandlers";
 import StartAlgorithmCard from "../../../Common/Algorithms/StartAlgorithmCard/StartAlgorithmCard";
 import GraphCard from "../../../Common/Algorithms/GraphCard/GraphCard";
 import GraphContext from "../../../../Contexts/Controller/Graph";
 import TopSortAlgorithmContext from "../.././../../Contexts/Controller/TopSort/Algorithm";
 
-const algorithmType = "General";
-const algorithmTitle = "TopSort";
-
 export default function TopSort({nodesRecord, setNodesRecord}) {
-    const nodesCardHandlers = getHandlers(nodesRecord, setNodesRecord);
+    const nodesCardHandlers = getNodesHandlers(setNodesRecord);
+    const startAlgorithmHandlers = getStartAlgorithmHandlers(setNodesRecord);
 
     return (
         <>
             <NodeContext.Provider value={{nodesRecord, handlers: nodesCardHandlers}}>
                 <NodesCard/>
             </NodeContext.Provider>
-            <TopSortAlgorithmContext.Provider value={{nodesRecord, handlers: nodesCardHandlers}}>
+            <TopSortAlgorithmContext.Provider value={{nodesRecord, handlers: startAlgorithmHandlers}}>
                 <StartAlgorithmCard/>
             </TopSortAlgorithmContext.Provider>
             <GraphContext.Provider value={{nodesRecord}}>
--- client/src/Utilities/algorithmHandlers.js
+++ client/src/Utilities/algorithmHandlers.js
@@ -1,23 +1,16 @@
 import { defaultNodeStyle } from "../Data/Algorithms/graph";
 
-export function getHandlers(nodes, setNodes) {
+export function getNodesHandlers(setNodes) {
     function addNode() {
         setNodes(nodes => nodes.addNode());
     }
 
-    function resetNodes() {
-        setNodes(nodesRecord => {
-            const newNodes = nodesRecord.get("nodes").map(node=>node.set("style", defaultNodeStyle));
-            return nodesRecord.set("nodes", newNodes);
-        });
+    function deleteNodeByIndex(i) {
+        setNodes(nodes => nodes.deleteNodeByIndex(i));
     }
 
     function setEdgeByIndex({to, from}, val) {
         setNodes(nodes => nodes.setEdgeByIndex({to, from}, val));
-    }
-
-    function deleteNodeByIndex(i) {
-        setNodes(nodes => nodes.deleteNodeByIndex(i));
     }
 
     function toggleIsDirected() {
@@ -28,9 +21,20 @@ export function getHandlers(nodes, setNodes) {
         setNodes(nodes => nodes.toggleIsWeighted());
     }
 
+    return {addNode, setEdgeByIndex, toggleIsDirected, toggleIsWeighted, deleteNodeByIndex};
+};
+
+export function getStartAlgorithmHandlers(setNodes) {
+    function resetNodes() {
+        setNodes(nodesRecord => {
+            const newNodes = nodesRecord.get("nodes").map(node=>node.set("style", defaultNodeStyle));
+            return nodesRecord.set("nodes", newNodes);
+        });
+    }
+
     function setNodesRecord(newRecord) {
         setNodes(newRecord);
     }
 
-    return {addNode, setNodesRecord, resetNodes, setEdgeByIndex, deleteNodeByIndex, toggleIsDirected, toggleIsWeighted};
+    return {setNodesRecord, resetNodes};
 };
