7743737db6b54dde7d14844a26c312941fb779ab
askenderski <skenderskia@gmail.com>
2022-08-19 15:22:16 +0300

    Fixed TopSort Algorithm context

--- client/src/Contexts/Controller/TopSort/Algorithm.js
+++ client/src/Contexts/Controller/TopSort/Algorithm.js
@@ -1,15 +1,22 @@
+import React from "react";
 import BasicAlgorithmContext from "../BasicAlgorithmContext";
 import {default as algorithmGetter} from "../../../Algorithms/General/TopSort/TopSort";
 import {default as controller} from "../../../Algorithms/General/TopSort/Controller";
 
-const AlgorithmContext = {
-    Provider: function({value, children}) {
-        return (
-            <BasicAlgorithmContext.Provider value={{...value, algorithmGetter, controller}}>
-                {children}        
-            </BasicAlgorithmContext.Provider>
-        );
-    }
+const AlgorithmContext = React.createContext();
+
+const OriginalProvider = AlgorithmContext.Provider;
+
+AlgorithmContext.Provider = function({value, children}) {
+    const contextValue = {...value, algorithmGetter, controller};
+
+    return (
+        <BasicAlgorithmContext.Provider value={contextValue}>
+            <OriginalProvider value={contextValue}>
+                {children}
+            </OriginalProvider>
+        </BasicAlgorithmContext.Provider>
+    );
 };
 
 export default AlgorithmContext;
