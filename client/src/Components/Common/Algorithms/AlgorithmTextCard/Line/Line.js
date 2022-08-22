a262bd3d39ca02a9a23fbbd3776f690807570466
askenderski <skenderskia@gmail.com>
2022-08-22 20:36:05 +0300

    Pointer now scrolled to if not on screen

--- client/src/Components/Common/Algorithms/AlgorithmTextCard/Line/Line.js
+++ client/src/Components/Common/Algorithms/AlgorithmTextCard/Line/Line.js
@@ -1,6 +1,15 @@
 import style from "./Line.module.css";
 import Pointer from "./Pointer";
+import {useEffect, useRef} from "react";
+import useOnScreen from "../../../../../Hooks/useOnScreen";
 
 export default function Line({children, point}) {
-    return <span className={style.line}><Pointer point={point}/>{children}</span>;
+    const ref = useRef();
+    const isOnScreen = useOnScreen(ref);
+
+    useEffect(()=>{
+        if (point && !isOnScreen) ref.current.scrollIntoView();
+    }, [point]);
+    
+    return <span ref={ref} className={style.line}><Pointer point={point}/>{children}</span>;
 }
