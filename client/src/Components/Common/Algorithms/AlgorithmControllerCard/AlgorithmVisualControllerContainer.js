cef23ebf0aab6073525830e9d1e32c8ee7c2a503
askenderski <skenderskia@gmail.com>
2022-09-11 23:31:39 +0300

    Considerations now scrolled into view

--- client/src/Components/Common/Algorithms/AlgorithmControllerCard/AlgorithmVisualControllerContainer.js
+++ client/src/Components/Common/Algorithms/AlgorithmControllerCard/AlgorithmVisualControllerContainer.js
@@ -1,9 +1,17 @@
+import { useEffect, useRef } from "react";
 import useAlgorithmVisualHandlers from "../../../../Hooks/useAlgorithmVisualHandlers";
 
 export default function AlgorithmVisualControllerContainer() {
     const {considerations} = useAlgorithmVisualHandlers();
 
-    return <ul>
+    const ref = useRef();
+
+    useEffect(()=>{
+        ref.current.scrollIntoView({behaviour: "smooth"});
+    }, [considerations]);
+
+    return <ul style={{overflow: "scroll"}}>
         {considerations.map(consideration=><li>{consideration.join(" ")}</li>)}
+        <li ref={ref}/>
     </ul>;
 }
